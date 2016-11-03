a170441f2ed31fc122ec4f0056729b136ca94736
Buuntu <gabriel.jabud@gmail.com>
2016-11-03 03:03:35 +0000

    Basic unix timestamp service, accepts normal dates and unix timestamps and outputs both

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('*', function (req, res) {
+  var dateStamp = {
+    unix: null,
+    date: null
+  };
+  
+  var timeStamp = decodeURIComponent(req.path).slice(1);
+  var dateIn = new Date(isNaN(timeStamp) ? timeStamp : +timeStamp);
+  if (dateIn.getTime()) {
+    dateStamp.unix = dateIn.getTime();
+    dateStamp.date = dateIn.toDateString();
+  }
+  
+  res.send(JSON.stringify(dateStamp));
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
 });
